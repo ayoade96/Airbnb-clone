2c3408a10ab689fd665c1f85652bcd42c61812a0
Ayoade Abdulrahman <abdulrahmanadesile@yahoo.com>
2023-05-04 10:58:29 +0100

    another fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic' 
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -5,7 +7,7 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export const dynamic = 'force-dynamic' 
+
 
 interface HomeProps {
   searchParams: IListingsParams
